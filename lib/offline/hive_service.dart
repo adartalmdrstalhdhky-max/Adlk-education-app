44363514b85f5217ec2f157c1cbef546e842db5e
adartalmdrstalhdhky-max <adartalmdrstalhdhky@gmail.com>
2025-12-03 23:50:54 +0300

    Update hive_service.dart

--- lib/offline/hive_service.dart
+++ lib/offline/hive_service.dart
@@ -1,46 +1,43 @@
 import 'package:hive/hive.dart';
-import '../offline/db_tables.dart';
+import 'package:hive_flutter/hive_flutter.dart';
+
+import 'models/student_model.dart';
+import 'models/teacher_model.dart';
+import 'models/book_model.dart';
+import 'models/lesson_model.dart';
+import 'models/question_model.dart';
+import 'models/answer_model.dart';
 
 class HiveService {
-  static const String studentBox = DBTables.students;
-  static const String teacherBox = DBTables.teachers;
-  static const String classBox = DBTables.classes;
-  static const String bookBox = DBTables.books;
+  static Future<void> initHive() async {
+    await Hive.initFlutter();
 
-  /// Initialize Hive boxes
-  Future<void> init() async {
-    await Hive.initFlutter(); // Requires hive_flutter package
-    await Hive.openBox(studentBox);
-    await Hive.openBox(teacherBox);
-    await Hive.openBox(classBox);
-    await Hive.openBox(bookBox);
-    print("Hive boxes initialized");
-  }
+    // تسجيل جميع أنواع النماذج
+    if (!Hive.isAdapterRegistered(1)) {
+      Hive.registerAdapter(StudentModelAdapter());
+    }
+    if (!Hive.isAdapterRegistered(2)) {
+      Hive.registerAdapter(TeacherModelAdapter());
+    }
+    if (!Hive.isAdapterRegistered(6)) {
+      Hive.registerAdapter(BookModelAdapter());
+    }
+    if (!Hive.isAdapterRegistered(7)) {
+      Hive.registerAdapter(LessonModelAdapter());
+    }
+    if (!Hive.isAdapterRegistered(8)) {
+      Hive.registerAdapter(QuestionModelAdapter());
+    }
+    if (!Hive.isAdapterRegistered(9)) {
+      Hive.registerAdapter(AnswerModelAdapter());
+    }
 
-  /// Add data to a box
-  Future<void> addData(String boxName, Map<String, dynamic> data) async {
-    var box = Hive.box(boxName);
-    await box.put(data['id'], data);
-    print("Data added to $boxName: $data");
-  }
-
-  /// Get all data from a box
-  List<Map<String, dynamic>> getAllData(String boxName) {
-    var box = Hive.box(boxName);
-    return box.values.cast<Map<String, dynamic>>().toList();
-  }
-
-  /// Update data in a box
-  Future<void> updateData(String boxName, String id, Map<String, dynamic> newData) async {
-    var box = Hive.box(boxName);
-    await box.put(id, newData);
-    print("Data updated in $boxName: $newData");
-  }
-
-  /// Delete data from a box
-  Future<void> deleteData(String boxName, String id) async {
-    var box = Hive.box(boxName);
-    await box.delete(id);
-    print("Data deleted from $boxName, id=$id");
+    // فتح الصناديق (Boxes) الأساسية
+    await Hive.openBox('students');
+    await Hive.openBox('teachers');
+    await Hive.openBox('books');
+    await Hive.openBox('lessons');
+    await Hive.openBox('questions');
+    await Hive.openBox('answers');
   }
 }
