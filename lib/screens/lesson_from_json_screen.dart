6601b1212965a1593af565de4a019c2761b5957b
عصام عبدالقوي احمد صالح <adartalmdrstalhdhky@gmail.com>
2025-12-19 17:01:52 +0300

    Create lesson_from_json_screen.dart

---
+++ lib/screens/lesson_from_json_screen.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+class LessonFromJsonScreen extends StatelessWidget {
+@override
+Widget build(BuildContext context) {
+return Scaffold(
+appBar: AppBar(title: Text('دروس النظام الرقمي')),
+body: Center(child: Text('يتم تحميل البيانات من ملف JSON')),
+);
+}
+}
