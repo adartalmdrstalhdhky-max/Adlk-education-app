1b83c8c26d4e982bb019a1af413f6e24540a84c2
عصام عبدالقوي احمد صالح <adartalmdrstalhdhky@gmail.com>
2025-12-19 18:40:58 +0300

    Update math_lessons_screen.dart

--- lib/screens/math_lessons_screen.dart
+++ lib/screens/math_lessons_screen.dart
@@ -1,11 +1,29 @@
-// math_lessons_screen.dart
 import 'package:flutter/material.dart';
-class MathLessonsScreen extends StatelessWidget {
+
+class MathLessonScreen extends StatelessWidget {
 @override
 Widget build(BuildContext context) {
 return Scaffold(
-appBar: AppBar(title: Text('دروس الرياضيات')),
-body: ListView(children: [ListTile(title: Text('الدرس الاول'), onTap: () {})]),
+appBar: AppBar(title: Text('عرض الدرس')),
+body: Padding(
+padding: EdgeInsets.all(16),
+child: Column(
+crossAxisAlignment: CrossAxisAlignment.start,
+children: [
+Text('عنوان الدرس الرياضيات الرقمية', style: TextStyle(fontSize: 22, fontWeight: FontWeight.bold)),
+SizedBox(height: 20),
+Container(
+height: 200,
+width: double.infinity,
+color: Colors.grey[300],
+child: Center(child: Text('منطقة عرض الفيديو او المحتوى')),
+),
+SizedBox(height: 20),
+Text('شرح الدرس لطلاب منارة الاجيال'),
+],
+),
+),
 );
 }
 }
+
