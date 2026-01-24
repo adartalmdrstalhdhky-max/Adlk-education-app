274dc58566b15972242a539a2ec6d5be4257b633
عصام عبدالقوي احمد صالح <adartalmdrstalhdhky@gmail.com>
2026-01-24 21:06:52 +0300

    Create Badge class with id, title, and description

---
+++ lib/domain/entities/badge.dart
@@ -0,0 +1,11 @@
+class Badge {
+  final String id;
+  final String title;
+  final String description;
+
+  Badge({
+    required this.id,
+    required this.title,
+    required this.description,
+  });
+}
