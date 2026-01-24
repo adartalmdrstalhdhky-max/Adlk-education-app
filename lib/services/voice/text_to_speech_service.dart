b581ca795fdb872ab53d5d0c16dc9c4cdea5cd69
عصام عبدالقوي احمد صالح <adartalmdrstalhdhky@gmail.com>
2026-01-24 21:48:30 +0300

    Create text_to_speech_service.dart

---
+++ lib/services/voice/text_to_speech_service.dart
@@ -0,0 +1,10 @@
+import 'package:flutter_tts/flutter_tts.dart';
+
+class TextToSpeechService {
+  final FlutterTts _tts = FlutterTts();
+
+  Future<void> speak(String text) async {
+    await _tts.setLanguage('ar');
+    await _tts.speak(text);
+  }
+}
