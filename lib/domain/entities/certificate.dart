b532ab723b2c25c66f0e22c02a2eda607b851a23
عصام عبدالقوي احمد صالح <adartalmdrstalhdhky@gmail.com>
2026-01-24 22:09:05 +0300

    Create certificate.dart

---
+++ lib/domain/entities/certificate.dart
@@ -0,0 +1,11 @@
+class Certificate {
+  final String studentName;
+  final String material;
+  final double score;
+
+  Certificate({
+    required this.studentName,
+    required this.material,
+    required this.score,
+  });
+}
