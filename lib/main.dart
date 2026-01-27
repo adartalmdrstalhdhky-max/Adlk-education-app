761052c6373eba13b0030e52423031fe7dbcb1c2
عصام عبدالقوي احمد صالح <adartalmdrstalhdhky@gmail.com>
2026-01-27 14:21:11 +0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'ui/screens/lesson_screen.dart';
+import 'ui/screens/parent_dashboard_screen.dart';
 
 void main() {
   runApp(const AdlkApp());
@@ -13,7 +14,64 @@ class AdlkApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Adlk',
-      home: LessonScreen(),
+      home: HomeSelector(),
+    );
+  }
+}
+
+class HomeSelector extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text("أدلك")),
+      body: Padding(
+        padding: const EdgeInsets.all(20),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            SizedBox(
+              width: double.infinity,
+              height: 60,
+              child: ElevatedButton(
+                child: const Text(
+                  "ابدأ التعلم",
+                  style: TextStyle(fontSize: 22),
+                ),
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (_) => LessonScreen(),
+                    ),
+                  );
+                },
+              ),
+            ),
+            const SizedBox(height: 20),
+            SizedBox(
+              width: double.infinity,
+              height: 60,
+              child: ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Colors.green,
+                ),
+                child: const Text(
+                  "لوحة ولي الأمر",
+                  style: TextStyle(fontSize: 22),
+                ),
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (_) => ParentDashboardScreen(),
+                    ),
+                  );
+                },
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
